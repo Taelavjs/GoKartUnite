d6c41ac2cb72a16da854718326002c24d080bd5d
taela Senior <taelavjs@hotmail.com>
2024-12-26 23:20:09 +0000

    Client side notifCounter

--- GoKartUnite/wwwroot/js/NotifBar.js
+++ GoKartUnite/wwwroot/js/NotifBar.js
@@ -1,1 +1,20 @@
-﻿
+﻿"use strict";
+
+
+const baseUrl = `${window.location.origin}/FollowTrack/GetNotifCount`;
+
+console.log('ID:', baseUrl);
+fetch(baseUrl, {
+    method: "POST"
+})
+    .then(response => {
+        if (!response.ok) {
+            throw new Error(`HTTP error! Status: ${response.status}`);
+        }
+        return response.json();
+    })
+    .then(data => {
+        console.log('Notification Count:', data);
+        $('#notifCount').text(data);
+
+    })
