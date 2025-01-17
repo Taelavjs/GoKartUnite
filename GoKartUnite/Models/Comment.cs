05b686708e8bc945c4ce77d5b8eb175bb1972ade
taela Senior <taelavjs@hotmail.com>
2025-01-17 14:17:22 +0000

    Comments stored with correct date

--- GoKartUnite/Models/Comment.cs
+++ GoKartUnite/Models/Comment.cs
@@ -5,7 +5,7 @@
         public int Id { get; set; }
         public string Text { get; set; }
 
-        public DateTime CreatedDate { get; set; }
+        public DateTime CreatedDate { get; set; } = DateTime.Now;
 
         public int AuthorId { get; set; }
 
