e34ac997d56f399f7b8d4613f9eef015ab453d2a
taela Senior <taelavjs@hotmail.com>
2025-03-26 17:41:50 +0000

    Graph has loading screen while waiting for AJAX request and has easing animations

--- GoKartUnite/wwwroot/js/GroupStatPage.js
+++ GoKartUnite/wwwroot/js/GroupStatPage.js
@@ -1,20 +1,18 @@
-﻿(async function () {
+﻿const loadingElement = document.getElementById("GroupStatGraphLoading");
+const graphElement = document.getElementById('acquisitions');
+(async function () {
+    document.getElementById("GroupStatGraphLoading").style.visibility = "hidden";
+
     var selectItem = document.getElementById("groupStatTrackSelect");
     selectItem.addEventListener("change", function () {
         console.log(selectItem);
         GroupMembersStatsRequest(selectItem.value);
     });
 
-
     document.addEventListener("DOMContentLoaded", function () {
         console.log(selectItem);
-
         GroupMembersStatsRequest(selectItem.value);
-
     });
-
-
-
 })();
 
 
@@ -22,61 +20,76 @@ const GroupMembersStatsRequest = (TrackTitle) => {
     const groupId = document.getElementById('acquisitions').dataset.groupid;
     console.log("Id : ", groupId, " Track : ", TrackTitle
     )
+
+    loadingElement.style.visibility = "visible";
+    const MIN_LOADING_TIME = 1000;
+
     $.ajax({
         url: `${window.location.origin}/group/GroupStats?GroupId=${groupId}&TrackTitle=${TrackTitle}`,
         contentType: "application/json; charset=utf-8",
         dataType: "json",
         success: (retObj) => {
-
-
-            const bestLapTimes = retObj.map(row => row.bestLapTime);
-            const minLapTime = Math.min(...bestLapTimes);
-            const maxLapTime = Math.max(...bestLapTimes);
-            const stepSize = Math.round((maxLapTime - minLapTime) / 10);
-
             if (Chart.getChart("acquisitions")) {
                 Chart.getChart("acquisitions")?.destroy()
             }
-            new Chart(
-                document.getElementById('acquisitions'),
-                {
-                    type: 'scatter',
-                    options: {
-                        scales: {
-                            y: {
-                                type: 'linear',
-                                ticks: {
-                                    min: minLapTime,
-                                    max: maxLapTime,
-                                    stepSize: stepSize,
-                                    callback: function (value) {
-                                        const minutes = Math.floor(value / 60000);
-                                        const seconds = ((value % 60000) / 1000).toFixed(0);
-                                        return minutes + ":" + (seconds < 10 ? "0" : "") + seconds;
-                                    }
+            setTimeout(() => {
+                loadingElement.style.visibility = "hidden";
+                const bestLapTimes = retObj.map(row => row.bestLapTime);
+                const minLapTime = Math.min(...bestLapTimes);
+                const maxLapTime = Math.max(...bestLapTimes);
+                const stepSize = Math.round((maxLapTime - minLapTime) / 10);
+                new Chart(
+                    graphElement,
+                    {
+                        type: 'scatter',
+                        options: {
+                            animation: {
+                                x: {
+                                    duration: 1500,
+                                    easing: 'easeOutQuad',
+                                    from: 0
+                                },
+                                y: {
+                                    duration: 1500,
+                                    easing: 'easeOutQuad',
+                                    from: 0
                                 }
                             },
-                            x: {
-                                type: "category",
-                                offset: true,
-
+                            scales: {
+                                y: {
+                                    type: 'linear',
+                                    ticks: {
+                                        min: minLapTime,
+                                        max: maxLapTime,
+                                        stepSize: stepSize,
+                                        callback: function (value) {
+                                            const minutes = Math.floor(value / 60000);
+                                            const seconds = ((value % 60000) / 1000).toFixed(0);
+                                            return minutes + ":" + (seconds < 10 ? "0" : "") + seconds;
+                                        }
+                                    }
+                                },
+                                x: {
+                                    type: "category",
+                                    offset: true,
+                                }
                             }
+                        },
+                        data: {
+                            labels: retObj.map(row => row.karterName),
+                            datasets: [
+                                {
+                                    label: `Best Lap Time At ${TrackTitle}`,
+                                    data: retObj.map(row => ({
+                                        x: row.karterName,
+                                        y: row.bestLapTime
+                                    }))
+                                }
+                            ]
                         }
-                    },
-                    data: {
-                        labels: retObj.map(row => row.karterName),
-                        datasets: [
-                            {
-                                label: `Best Lap Time At ${TrackTitle}`,
-                                data: retObj.map(row => ({
-                                    x: row.karterName,
-                                    y: row.bestLapTime
-                                }))
-                            }
-                        ]
                     }
-                }
-            );
+                );
+            }, MIN_LOADING_TIME);
         }
     })
 }
