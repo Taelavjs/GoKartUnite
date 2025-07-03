651137fb274c0ab3cb66a54e489031e1262a133f
taela Senior <taelavjs@hotmail.com>
2025-07-03 16:06:11 +0100

    Furhter tested friend requests and status

--- UnitTesting/ControllerTests/ProfileTests.cs
+++ UnitTesting/ControllerTests/ProfileTests.cs
@@ -13,13 +13,12 @@ namespace UnitTesting.ControllerTests
     {
         public ProfileTests(TestServer<Program> server) : base(server) { }
 
+        // Friend Request : Add ******************************************
+
         [Fact]
         public async Task UserAddsFriends_ShowsPendingRequestSent()
         {
             // Arrange
-
-            var response3 = await _client.GetAsync("/karterHome/Details");
-
             var formData = new Dictionary<string, string>
             {
                 { "friendId", otherUserId.ToString() },
@@ -38,5 +37,196 @@ namespace UnitTesting.ControllerTests
             // Assert
             Assert.Equal("Cancel", newFriendStatus);  // Because Add returns NewFriendStatus = "Cancel"
         }
+
+        [Fact]
+        public async Task UserAddsFriends_AlreadyFriends_Error()
+        {
+            // Arrange
+            _dbContext.Friendships.Add(new GoKartUnite.Models.Friendships
+            {
+                accepted = true,
+                KarterFirstId = userId,
+                KarterSecondId = otherUserId,
+            });
+            _dbContext.SaveChanges();
+            var formData = new Dictionary<string, string>
+            {
+                { "friendId", otherUserId.ToString() },
+                { "friendAction", "Add" }
+            };
+            var content = new FormUrlEncodedContent(formData);
+
+            // Act
+            var response = await _client.PostAsync("/KarterHome/HandleFriendRequest", content);
+            Assert.Equal(System.Net.HttpStatusCode.BadRequest, response.StatusCode);
+        }
+
+        // ******************************************************************
+
+        // Friend Request : Accept ******************************************
+
+        [Fact]
+        public async Task UserAcceptsFriends_ShowsNewFriendAdded()
+        {
+            // Arrange
+
+            _dbContext.Friendships.Add(new GoKartUnite.Models.Friendships
+            {
+                accepted = false,
+                KarterFirstId = userId,
+                KarterSecondId = otherUserId,
+                requestedByInt = otherUserId
+            });
+            _dbContext.SaveChanges();
+            var formData = new Dictionary<string, string>
+            {
+                { "friendId", otherUserId.ToString() },
+                { "friendAction", "Accept" }
+            };
+            var content = new FormUrlEncodedContent(formData);
+
+            // Act
+            var response = await _client.PostAsync("/KarterHome/HandleFriendRequest", content);
+            Assert.Equal(System.Net.HttpStatusCode.OK, response.StatusCode);
+            var responseString = await response.Content.ReadAsStringAsync();
+            var jsonDoc = System.Text.Json.JsonDocument.Parse(responseString);
+            var root = jsonDoc.RootElement;
+            var newFriendStatus = root.GetProperty("newFriendStatus").GetString();
+
+            // Assert
+            Assert.Equal("Remove", newFriendStatus);
+        }
+
+        [Fact]
+        public async Task UserAcceptsFriends_AlreadyFriends_Error()
+        {
+            // Arrange
+            _dbContext.Friendships.Add(new GoKartUnite.Models.Friendships
+            {
+                accepted = true,
+                KarterFirstId = userId,
+                KarterSecondId = otherUserId,
+                requestedByInt = otherUserId
+            });
+            _dbContext.SaveChanges();
+            var formData = new Dictionary<string, string>
+            {
+                { "friendId", otherUserId.ToString() },
+                { "friendAction", "Accept" }
+            };
+            var content = new FormUrlEncodedContent(formData);
+
+            // Act
+            var response = await _client.PostAsync("/KarterHome/HandleFriendRequest", content);
+            Assert.Equal(System.Net.HttpStatusCode.BadRequest, response.StatusCode);
+        }
+
+        // ******************************************************************
+
+        // Friend Request : Withdraws Request ******************************************
+
+        [Fact]
+        public async Task UserWithdrawsRequest_ShowsNoPendingRequests()
+        {
+            // Arrange
+
+            _dbContext.Friendships.Add(new GoKartUnite.Models.Friendships
+            {
+                accepted = false,
+                KarterFirstId = userId,
+                KarterSecondId = otherUserId,
+                requestedByInt = userId
+            });
+            _dbContext.SaveChanges();
+            var formData = new Dictionary<string, string>
+            {
+                { "friendId", otherUserId.ToString() },
+                { "friendAction", "Cancel" }
+            };
+            var content = new FormUrlEncodedContent(formData);
+
+            // Act
+            var response = await _client.PostAsync("/KarterHome/HandleFriendRequest", content);
+            Assert.Equal(System.Net.HttpStatusCode.OK, response.StatusCode);
+            var responseString = await response.Content.ReadAsStringAsync();
+            var jsonDoc = System.Text.Json.JsonDocument.Parse(responseString);
+            var root = jsonDoc.RootElement;
+            var newFriendStatus = root.GetProperty("newFriendStatus").GetString();
+
+            // Assert
+            Assert.Equal("Add", newFriendStatus);
+        }
+
+        [Fact]
+        public async Task UserWithdraw_AlreadyFriends_RemovesAsFriendSuccessfully()
+        {
+            var exList = _dbContext.Friendships.ToList();
+            if (exList.Count > 0)
+            {
+                Assert.Equal(false, true);
+            }
+            // Arrange
+            _dbContext.Friendships.Add(new GoKartUnite.Models.Friendships
+            {
+                accepted = true,
+                KarterFirstId = userId,
+                KarterSecondId = otherUserId,
+                requestedByInt = userId
+            });
+            _dbContext.SaveChanges();
+            var formData = new Dictionary<string, string>
+            {
+                { "friendId", otherUserId.ToString() },
+                { "friendAction", "Cancel" }
+            };
+            var content = new FormUrlEncodedContent(formData);
+            var NotificationsForOtherKarter = _dbContext.FriendStatusNotifications.Where(x => x.UserId == otherUserId).ToList();
+            var ddd = _dbContext.FriendStatusNotifications.ToList();
+
+            // Act
+            var response = await _client.PostAsync("/KarterHome/HandleFriendRequest", content);
+            Assert.Equal(System.Net.HttpStatusCode.OK, response.StatusCode);
+
+
+        }
+
+        // ******************************************************************
+
+
+        // Friend Request : Remove Friend ******************************************
+
+        [Fact]
+        public async Task UserRemoveFriend_ShowsNoFriends()
+        {
+            // Arrange
+
+            _dbContext.Friendships.Add(new GoKartUnite.Models.Friendships
+            {
+                accepted = true,
+                KarterFirstId = userId,
+                KarterSecondId = otherUserId,
+                requestedByInt = userId
+            });
+            _dbContext.SaveChanges();
+            var formData = new Dictionary<string, string>
+            {
+                { "friendId", otherUserId.ToString() },
+                { "friendAction", "Remove" }
+            };
+            var content = new FormUrlEncodedContent(formData);
+
+            // Act
+            var response = await _client.PostAsync("/KarterHome/HandleFriendRequest", content);
+            Assert.Equal(System.Net.HttpStatusCode.OK, response.StatusCode);
+            var responseString = await response.Content.ReadAsStringAsync();
+            var jsonDoc = System.Text.Json.JsonDocument.Parse(responseString);
+            var root = jsonDoc.RootElement;
+            var newFriendStatus = root.GetProperty("newFriendStatus").GetString();
+
+            // Assert
+            Assert.Equal("Add", newFriendStatus);
+        }
+
+        // ******************************************************************
     }
 }
--- UnitTesting/FakeAuthHandler.cs
+++ UnitTesting/FakeAuthHandler.cs
@@ -8,6 +8,7 @@ using System.Security.Claims;
 using System.Text;
 using System.Text.Encodings.Web;
 using System.Threading.Tasks;
+[assembly: CollectionBehavior(DisableTestParallelization = true)]
 
 namespace UnitTesting
 {
